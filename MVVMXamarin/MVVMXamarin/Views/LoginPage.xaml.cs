cd3afd3fd3669c2a3034230c3ad24a303d5383ec
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-08-27 12:50:00 -0400

    HomePage completed using MVVM

---
+++ MVVMXamarin/MVVMXamarin/Views/LoginPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using MVVMXamarin.ViewModels;
+using MVVMXamarin.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new LoginViewModel(new AlertService(), new NavigationService());
+        }
+    }
+}
