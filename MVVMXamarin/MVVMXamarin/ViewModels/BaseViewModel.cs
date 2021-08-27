fa2f3cdc1989562995604d9f931ceeec8172ae42
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-08-27 12:24:24 -0400

    fixed HomePage and UI placement

--- MVVMXamarin/MVVMXamarin/ViewModels/BaseViewModel.cs
+++ MVVMXamarin/MVVMXamarin/ViewModels/BaseViewModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
-
+using MVVMXamarin.Services;
 
 namespace MVVMXamarin.ViewModels
 {
--- MVVMXamarin/MVVMXamarin/ViewModels/HomeViewModel.cs
+++ MVVMXamarin/MVVMXamarin/ViewModels/HomeViewModel.cs
@@ -16,10 +16,25 @@ namespace MVVMXamarin.ViewModels
         public MenuOption FastFoodsMenuOption { get; set; } = new MenuOption();
         public MenuOption FeaturedFoodsMenuOption { get; set; } = new MenuOption();
 
-    }
-        
         public HomeViewModel(IAlertService alertService, INavigationService navigationService) : base(alertService, navigationService)
         {
-            
+            BarAndHotelsMenuOption.Title = "Bars & Hotels";
+            BarAndHotelsMenuOption.Image = "BarsIcon";
+
+            FreeDinningMenuOption.Title = "Free Dinning";
+            FreeDinningMenuOption.Image = "FreeDiningIcon";
+
+            CafesMenuOption.Title = "Cafes";
+            CafesMenuOption.Image = "CafeIcon";
+
+            NearbyMenuOption.Title = "Nearby";
+            NearbyMenuOption.Image = "NearbyIcon";
+
+            FastFoodsMenuOption.Title = "Fast Foods";
+            FastFoodsMenuOption.Image = "FastFoodIcon";
+
+            FeaturedFoodsMenuOption.Title = "Featured Foods";
+            FeaturedFoodsMenuOption.Image = "FeaturedFoodIcon";
         }
+    }
 }
