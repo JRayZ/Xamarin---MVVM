d80f7f4ab2b38b7bc40bcc63b33c1eede333e1bf
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-08-27 11:49:02 -0400

    making HomePage content

---
+++ MVVMXamarin/MVVMXamarin/Views/HomePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMXamarin.Services;
+using MVVMXamarin.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            BindingContext = new HomeViewModel(new AlertService(), new NavigationService());
+        }
+    }
+}
